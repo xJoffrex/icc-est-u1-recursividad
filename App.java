e36f7a15355108f066540d2357249c04700ac0cc
xJoffrex <mguaricelac@gmail.com>
2025-04-10 19:16:51 -0500

    ejercicios de recursividad

--- App.java
+++ App.java
@@ -1,8 +1,27 @@
 public class App {
     public static void main(String[] args) {
-        int n = 5;
+        int n = 6;
+
         int resultadoFinal = factorial(n);
-        System.out.println("Resultado: " + n);
+        System.out.println("Resultado: " + resultadoFinal);
+
+
+        int base = 2;
+        
+        int exponente = 3;
+        ejerciciosRecursivos ejercicios = new ejerciciosRecursivos(); 
+
+        int resultadoConsecutivos = ejercicios.sumaConsecutivos(n);
+        System.out.println("La suma total es: " + resultadoConsecutivos);
+
+        int resultadoPotencia = ejercicios.getPotencia(base, exponente);
+        System.out.println("Potencia de " + base + " elevado a " + exponente + ": " + resultadoPotencia);
+
+        int resultadoSuma = ejercicios.Sumatoria2(n);
+        System.out.println("La suma de todo es "+resultadoSuma);
+
+
+
     }
         
     public static int factorial(int n){
@@ -12,7 +31,7 @@ public class App {
         }
         int resultadoParcial = n * factorial(n-1);
         System.out.println("Calculando Factorial de: " + n + " Factorial (" + (n - 1) + " - 1)");
-         return resultadoParcial;
+        return resultadoParcial;
     }
         
 }
--- ejerciciosRecursivos.java
+++ ejerciciosRecursivos.java
@@ -7,12 +7,27 @@ public class ejerciciosRecursivos {
     }
 
     public int sumaConsecutivos(int n){
-        if (n == 1){
-            return 1;
-        }  
-        int resultadoParcial = sumaConsecutivos(n-1);
-        int resultado = n + resultadoParcial;
-        return resultado;
+    
+        if (n == 1) {
+            return 1; 
+        }
+        return n + sumaConsecutivos(n - 1); 
     }
+
+    public int getPotencia(int base, int exponente){
+        if (exponente == 0){
+            return 1;
+        }else{
+            return base * getPotencia(base, exponente-1);
+        }      
+    }
+
+    public int Sumatoria2(int n){
+        if (n <= 10){
+            return n;
+        }
+        return (n%10) + Sumatoria2(n/10);
+    }
+
 }
 
